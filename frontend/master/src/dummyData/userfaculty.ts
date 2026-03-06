3d36016f7d272cb88598e23719171ba9602346b3
Sathyajothi-05-03 <sathyajothi.yoho@gmail.com>
2026-03-06 20:08:44 +0530

    userfaculty

---
+++ frontend/master/src/dummyData/userfaculty.ts
@@ -0,0 +1,52 @@
+import usercard1 from "../assets/userfaculty/usercard1.png"
+import usercard2 from "../assets/userfaculty/usercard2.png"
+import usercard3 from "../assets/userfaculty/usercard3.png"
+import usercard4 from "../assets/userfaculty/usercard3.png"
+
+export const UserCard = [
+    {
+        id:1,
+        icon: usercard1,
+        value: 3,
+        title: "HODs"
+    },
+    {
+        id:2,
+        icon: usercard2,
+        value:5,
+        title: "Staff"
+    },
+    {
+        id:3,
+        icon: usercard3,
+        value: 5,
+        title: "Tele-Callers"
+    },
+    {
+        id:4,
+        icon: usercard4,
+        value: 1,
+        title: "Receptionists"
+    }
+]
+
+export const UserDetail =[
+    {
+        id:1,
+        name:"Priya Sharma",
+        email:"priya@gmail.com",
+        role: "staff"
+    },
+    {
+        id:2,
+        name:"Sharma hjbk",
+        email:"priya@gmail.com",
+        role: "HOD"
+    },
+    {
+        id:3,
+        name:"Krishnan hjbk",
+        email:"priya@gmail.com",
+        role: "telecaller"
+    },
+]
--- frontend/master/src/pages/UsersAndFaculty.tsx
+++ frontend/master/src/pages/UsersAndFaculty.tsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import { useState } from 'react'
+import {UserCard} from "../dummyData/userfaculty"
+import plus from "../assets/userfaculty/plus.png"
+import CreateUserFaculty from '../components/userFaculty/CreateUserFaculty'
+import {UserDetail} from "../dummyData/userfaculty"
+import editicon from "../assets/userfaculty/edit.png"
+import deleteicon from "../assets/userfaculty/delete.png"
+import viewicon from "../assets/userfaculty/view.png"
+import search from "../assets/userfaculty/search.png"
 
 const UsersAndFaculty = () => {
+  const [createshowmodel, setcreateshowmodel] = useState(false)
+  const [user, setuser] = useState(UserDetail)
+  const [searchTerm, setSearchTerm] = useState("")
+  const adduser = (newuser:any) => {
+    setuser([...user, newuser])
+  }
+
+  const [selectRole, setselectRole] = useState("ALL ROLES")
+  const FilterRole = user.filter((ListUserData)=>{
+    const MatchRole = selectRole == "ALL ROLES" || ListUserData.role.toUpperCase() === selectRole.toUpperCase()
+    const MatchSearch =ListUserData.name.toLowerCase().includes(searchTerm.toLowerCase()) || ListUserData.email.toLowerCase().includes(searchTerm.toLowerCase())
+    return MatchRole && MatchSearch
+  })
+
   return (
-    <div>
-      Users and Faculty
+    <div className='overflow-hidden flex flex-col gap-5'>
+      <div className='flex justify-between'>
+        <div>
+          <h1 className='font-bold text-xl'>User & Faculty Management</h1>
+          <p>Create and manage all institute users with complete control</p>
+        </div>
+        <div className='border rounded-md px-2 flex gap-2 bg-[#54191D]'>
+          <div className={`flex items-center justify-center text-center`}>
+            <img src={plus} className='w-6 h-6'/>
+          </div>
+          <button onClick={()=>{setcreateshowmodel(true)}} className='text-[#EDBF5C] cursor-pointer'>Create User&Faculty</button>
+        </div>
+      </div>
+
+      <div className='grid sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-5'>
+        {UserCard.map((data,index)=>{
+          return(
+            <div key={index} className=' bg-[#FFFFFF] shadow-[0_0_15px_rgba(0,0,0,0.1)] p-4 grid gap-2 rounded-xl'>
+              <div className='flex justify-between gap-3'>
+                <div className={`rounded-md p-2 ${data.title === "HODs"? "bg-[#FBE7BB]" : data.title === "Staff"? "bg-[#FBE7BB]" : data.title === "Tele-Callers"? "bg-[#DBEAFE]" : data.title === "Receptionists"? "bg-[#DCFCE7]" : " bg-red-200"}`}>
+                  <img src={data.icon} className='w-6 h-6'/>
+                </div>
+                <div className='font-bold text-xl'>{data.value}</div>
+              </div>
+              <div>
+                {data.title}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      <div className=' p-2 rounded-xl bg-[#FFFFFF] shadow-[0_0_15px_rgba(0,0,0,0.1)]'>
+        <div className='flex flex-col md:flex-row gap-4'>
+          <div className='w-full md:w-[70%] xl:w-[90%] grid md:gap-5 gap-2'>
+            <div>
+              <h1 className='font-bold'>Search Staff</h1>
+            </div>
+            <div className='border border-[#4A5565] rounded-md flex gap-2 p-2'>
+              <img src={search} className='w-5 h-5 mt-2'/>
+              <div className=' rounded-md flex gap-2 p-2 w-full'>
+                <input type="text" name="search" placeholder="Search by name, email, phone etc..." className="outline-none w-full" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+            </div>
+            </div>
+          </div>
+          <div className='w-full md:w-[30%] xl:w-[20%] grid md:gap-3 gap-2'>
+            <div>
+              <h1 className='font-bold'>All Rolls</h1>
+            </div>
+            <select value={selectRole} onChange={(e)=>setselectRole(e.target.value)} className='border border-[#4A5565]  rounded-md p-2'>
+              <option value="ALL ROLES">All roles</option>
+              <option value="HOD">HOD</option>
+              <option value="STAFF">Staff</option>
+              <option value="TELECALLER">Telecaller</option>
+              <option value="RECEPTIONIST">Receptionist</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className='rounded-xl p-2  bg-[#FFFFFF] shadow-[0_0_15px_rgba(0,0,0,0.1)]'>
+        <div className=' bg-[#FFFFFF] shadow-[0_0_15px_rgba(0,0,0,0.1)] rounded-md px-4 **:text-md py-2 **:font-bold flex justify-between'>
+          <h1>NAME</h1>
+          <h1>EMAIL</h1>
+          <h1>ROLE</h1>
+          <h1>ACTIONS</h1>
+        </div>
+
+        <div className=' rounded-md mt-4 px-5 py-2 flex flex-col justify-between '>
+          {FilterRole.map((data,index)=>{
+            return(
+              <div key={index} className='flex justify-between border-b pb-5 mb-5'>
+                  <p className='text-sm'>{data.name}</p>
+                  <p className='text-sm'>{data.email}</p>
+                  <p className='text-sm'>{data.role}</p>
+                <div className='flex gap-2'>
+                  <div className='rounded-md p-1 bg-[#008BBF]'>
+                    <img src={editicon} className='w-4 h-4'/>
+                  </div>
+                  <div className='rounded-md p-1 bg-[#FF4C4C]'>
+                    <img src={deleteicon} className='w-4 h-4'/>
+                  </div>
+                  <div className='rounded-md p-1 bg-[#20D432]'>
+                    <img src={viewicon} className='w-4 h-4'/>
+                  </div>
+                </div>
+              </div>
+            )
+          })}    
+        </div>
+      </div>
+
+      <div >
+        {createshowmodel && 
+        <CreateUserFaculty setcreateshowmodel={setcreateshowmodel} adduser={adduser} />}
+      </div>
     </div>
   )
 }
