23884efd1c64d52da88e49f539b3a4c46debf3cd
Sathyajothi-05-03 <sathyajothi.yoho@gmail.com>
2026-03-09 19:55:24 +0530

    meeting

---
+++ frontend/master/src/dummyData/meeting.ts
@@ -0,0 +1,51 @@
+import { Phone } from "lucide-react";
+
+
+export const Appointments =[
+    {
+        id:1,
+        name:"Mike Johnson",
+        status:"Ongoing",
+        Phone:"8901234893",
+        priority:"High",
+        purpose:"Mock Interview",
+        Time:"10:30 AM"
+    },
+    {
+        id:2,
+        name:"Sarah Johnson",
+        status:"Scheduled",
+        Phone:"2953334893",
+        priority:"Normal",
+        purpose:"Corse Enquiry",
+        Time:"12:30 PM"
+    },
+
+]
+
+export const OverAllData =[
+    {
+        meetingId:"M001",
+        visitorName: "Priya sharma",
+        purpose: "Course Inquiry-Data Science",
+        time: "10:00 AM",
+        priority: "High",
+        status: "Ongoing"
+    },
+    {
+        meetingId:"M002",
+        visitorName: "Vikram-Desai",
+        purpose: "Fee Discussion",
+        time: "11:00 AM",
+        priority: "Normal",
+        status: "Pending"
+    },
+    {
+        meetingId:"M003",
+        visitorName: "sharma Dhevi",
+        purpose: "Admission Process Inquiry",
+        time: "11:30 AM",
+        priority: "High",
+        status: "Ongoing"
+    },
+]
