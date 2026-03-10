43cc5264e8fa7b55a57179517918c4c2891c25ad
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2026-03-10 20:06:47 +0530

    updated

--- frontend/telecalling/src/services/RTKQuery/CallerQueryApi.ts
+++ frontend/telecalling/src/services/RTKQuery/CallerQueryApi.ts
@@ -1,56 +1,64 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import ApiLists from "../ApiLists";
+import { GetLocalStorage } from "../../utils/LocalStorage";
 
 export const CallerQueryApi = createApi({
-    reducerPath:'callerquery',
+    reducerPath: 'callerquery',
     baseQuery: fetchBaseQuery({
         baseUrl: import.meta.env.VITE_BACKEND_URL,
+        prepareHeaders: (headers) => {
+            const token = GetLocalStorage('t_a_tk')
+            if (token) {
+                headers.set('authorization', `${token}`)
+            }
+            return headers
+        }
     }),
-    tagTypes: ['leads','compleads','course','batch'],
-    endpoints: (builder)=>({
+    tagTypes: ['leads', 'compleads', 'course', 'batch'],
+    endpoints: (builder) => ({
         getEmployeeLeads: builder.query({
-            query:({uuid,status})=>({
-                url:ApiLists.leads.getByemp.replace(':uuid',uuid),
-                params:{status},
+            query: ({ uuid, status }) => ({
+                url: ApiLists.leads.getByemp.replace(':uuid', uuid),
+                params: { status },
             }),
-            providesTags:['leads'],
+            providesTags: ['leads'],
         }),
         updateEmployeeLeads: builder.mutation({
-            query:(data)=>({
-                url: ApiLists.leads.putStatus.replace(':uuid',data.uuid),
-                method:'PUT',
-                body:data,
+            query: (data) => ({
+                url: ApiLists.leads.putStatus.replace(':uuid', data.uuid),
+                method: 'PUT',
+                body: data,
             }),
             invalidatesTags: ['compleads']
         }),
         getEmployeeCompletedLeads: builder.query({
-            query:(data)=>ApiLists.leads.completeLeads.replace(':uuid',data.uuid),
-            providesTags:['compleads']
+            query: (data) => ApiLists.leads.completeLeads.replace(':uuid', data.uuid),
+            providesTags: ['compleads']
         }),
-        getTeleCallerStatusLeads:builder.query({
-            query:(uuid)=>ApiLists.leads.getBystatus.replace(':uuid',uuid),
+        getTeleCallerStatusLeads: builder.query({
+            query: (uuid) => ApiLists.leads.getBystatus.replace(':uuid', uuid),
         }),
-        createStudentAdmission:builder.mutation({
-            query:(data)=>({
-                url:ApiLists.admission.student,
-                method:'POST',
-                body:data,
+        createStudentAdmission: builder.mutation({
+            query: (data) => ({
+                url: ApiLists.admission.student,
+                method: 'POST',
+                body: data,
             })
         }),
-        createPaymentAdmission:builder.mutation({
-            query:(data)=>({
-                url:'/telecalling/payment/create',
-                method:'POST',
-                body:data
+        createPaymentAdmission: builder.mutation({
+            query: (data) => ({
+                url: '/telecalling/payment/create',
+                method: 'POST',
+                body: data
             })
         }),
-        getAllcourse:builder.query({
-            query:()=>ApiLists.admission.getallcourse,
-            providesTags:['course']
+        getAllcourse: builder.query({
+            query: () => ApiLists.admission.getallcourse,
+            providesTags: ['course']
         }),
-        getAllBatch:builder.query({
-            query:(uuid)=>ApiLists.admission.getbatchBycourse.replace(':uuid',uuid),
-            providesTags:['batch']
+        getAllBatch: builder.query({
+            query: (uuid) => ApiLists.admission.getbatchBycourse.replace(':uuid', uuid),
+            providesTags: ['batch']
         })
     })
 })
--- frontend/telecalling/src/services/api.ts
+++ frontend/telecalling/src/services/api.ts
@@ -1,57 +1,65 @@
-import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import ApiLists from "./ApiLists"
+import { GetLocalStorage } from "../utils/LocalStorage"
 
 export const CommonApi = createApi({
-    reducerPath:'common',
-    baseQuery:fetchBaseQuery({
+    reducerPath: 'common',
+    baseQuery: fetchBaseQuery({
         baseUrl: import.meta.env.VITE_BACKEND_URL,
+        prepareHeaders: (headers) => {
+            const token = GetLocalStorage('t_a_tk')
+            if (token) {
+                headers.set('authorization', `${token}`)
+            }
+            return headers
+        }
     }),
-    tagTypes:['commonapi','leads'],
-    endpoints:(builder)=>({
+    tagTypes: ['commonapi', 'leads'],
+    endpoints: (builder) => ({
         getDashboard: builder.query({
-            query:()=> ApiLists.common.getAdminDash,
+            query: () => ApiLists.common.getAdminDash,
         }),
 
         getEmployeeStatus: builder.query({
-            query:()=>'/telecalling/emp-status'
+            query: () => '/telecalling/emp-status'
         }),
 
         uploadLeads: builder.mutation({
-            query:(formData)=>({
+            query: (formData) => ({
                 url: ApiLists.leads.postUpload,
-                method:"POST",
-                body:formData
+                method: "POST",
+                body: formData
             })
         }),
         getAllTeleCallersList: builder.query({
-            query:()=>ApiLists.common.getAllTele,
+            query: () => ApiLists.common.getAllTele,
         }),
-        AssignLeads:builder.mutation({
-            query:(data)=>({
-                url:ApiLists.leads.postAssign,
-                method:'POST',
-                body:data,
+        AssignLeads: builder.mutation({
+            query: (data) => ({
+                url: ApiLists.leads.postAssign,
+                method: 'POST',
+                body: data,
             })
         }),
         getAllLeads: builder.query({
-            query:()=>ApiLists.leads.getAll,
-            providesTags:['leads']
+            query: () => ApiLists.leads.getAll,
+            providesTags: ['leads']
         }),
-        TelecallerLogin:builder.mutation({
-            query:(data)=>({
-                url:data?.isAdmin ? ApiLists.login.admin : ApiLists.login.tele,
-                method:'POST',
-                body:data
+        TelecallerLogin: builder.mutation({
+            query: (data) => ({
+                url: data?.isAdmin ? ApiLists.login.admin : ApiLists.login.tele,
+                method: 'POST',
+                body: data
             })
         }),
-         getActiveEmployee:builder.query({
-            query:()=>ApiLists.common.getactiveemp,
+        getActiveEmployee: builder.query({
+            query: () => ApiLists.common.getactiveemp,
         }),
-        getRecentAdmission:builder.query({
-            query:()=>ApiLists.common.getrecetamit,
+        getRecentAdmission: builder.query({
+            query: () => ApiLists.common.getrecetamit,
         }),
-        getAllPayments:builder.query({
-            query:()=>ApiLists.common.getAllpayment
+        getAllPayments: builder.query({
+            query: () => ApiLists.common.getAllpayment
         })
     })
 })
